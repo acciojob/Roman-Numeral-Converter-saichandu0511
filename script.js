49b6bdb8f3f10d2e2376ca37fa3c80d98145eb98
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-15 21:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ function convertToRoman(num) {
       6:['I', 1]
     };
   //your code here
+  function convertToRoman(num) {
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
+
   let result = "";
 
   for (let i = 0; i < 7; i++) {
